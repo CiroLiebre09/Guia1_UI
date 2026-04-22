c63b9c797a3f7bc931bd03d526e603ed92b87070
CiroLiebre09 <95586291+CiroLiebre09@users.noreply.github.com>
2026-04-22 10:41:39 -0300

    Initial commit

---
+++ Assets/Scripts/MayoriaDeEdadUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MayoriaDeEdadUI : MonoBehaviour
+{
+
+    //Crear los elementos de UI para permitir al usuario ingresar dos nros enteros y que al presionar un bot muestre en pantalla el resultado de la suma de los nros ingresados
+    //Incluir un titulo de UI y (opcioal) si alguno de los numeros ingresados es negtivo mostrar error en el resultado 
+
+    public TMP_InputField input_IngresoEdad;
+    public TextMeshProUGUI txt_Resultado;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void CheckLegalAge()
+    {
+        int userAge = int.Parse(input_IngresoEdad.text);
+        if(userAge < 18)
+        {
+            txt_Resultado.text = "No es mayor";
+        }
+        else
+        {
+            txt_Resultado.text = "Es mayor";
+        }
+    }
+}
---
+++ Assets/Scripts/Suma.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Suma : MonoBehaviour
+{
+    public TMP_InputField input_suma1;
+    public TMP_InputField input_suma2;
+    public TextMeshProUGUI txt_Resultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Sumar()
+    {
+        int parsearNum1 = int.Parse(input_suma1.text);
+        int parsearNum2 = int.Parse(input_suma2.text);
+        int suma = parsearNum1 + parsearNum2;
+        string resultado = (suma).ToString();
+        txt_Resultado.text = "el resultado de la suma entre los dos números ingresados es " + resultado;
+    } 
+}
